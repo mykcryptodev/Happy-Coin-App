4400ba9e4d446c6030aadef5a1b632904596d6d7
Myk <89474773+mykcryptodev@users.noreply.github.com>
2021-09-03 12:13:33 -0400

    fade posts in

--- src/components/post/List.vue
+++ src/components/post/List.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+    <transition name="slide-fade-down-slow">
+      <v-row v-if="isLoading" class="d-flex justify-center">
+        <v-col v-for="n in 5" :key="n" cols="12">
+          <router-link :to="{ name: 'Post', params: { id: n } }">
+            <v-card shaped>
+              <v-skeleton-loader class="pl-3 pt-3" type="heading"></v-skeleton-loader>
+              <v-card-text>
+                <v-skeleton-loader class="pb-2" type="image"></v-skeleton-loader>
+                <v-skeleton-loader type="paragraph"></v-skeleton-loader>
+              </v-card-text>
+            </v-card>
+          </router-link>
+        </v-col>
+      </v-row>
+    </transition>
     <v-row class="d-flex justify-center">
       <transition-group name="slide-fade-down-slow">
         <v-col v-for="post in posts" :key="post.id">
@@ -25,6 +40,9 @@ export default {
   },
 
   computed: {
+    isLoading () {
+      return this.posts.length === 0
+    },
     posts () {
       return this.$store.getters.getPosts
     }
